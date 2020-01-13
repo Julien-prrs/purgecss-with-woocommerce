1d52c990b588cd930836ebf7c591e762ad5bf565
Julien-prrs <perros.julien@outlook.fr>
2020-01-13 21:57:27 +0100

    Adding whitelist items

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 module.exports = {
     whitelist: [
         'woocommerce',
-        'add_payment_method'
+        'add_payment_method',
+        'related',
+        'button',
+        'variable_product_options'
     ],
     whitelistPatterns: [
-        /woocommerce-/,
-        /wc-/
-        // '.woocommerce-*',
-        // '.wc-*'
+        /^woocommerce-/,
+        /^wc-/,
+        /^wc_ /,
+        /^product-/,
+        /^columns-/,
     ]
 }
